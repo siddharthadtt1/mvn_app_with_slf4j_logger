776c3bbb60f2de339b3128bee30f1ac2313de8df
siddharthadtt1 <siddharthadtt1@gmail.com>
2017-12-08 17:47:54 +0530

    Added slf4j-logger dependency example

---
+++ src/main/java/com/emc/test_logger/App.java
@@ -0,0 +1,16 @@
+package com.emc.test_logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static Logger logger = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Hello world ..");
+	}
+}
